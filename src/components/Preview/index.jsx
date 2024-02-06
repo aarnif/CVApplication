bf47da45a4bae1eecaa94b5c9cac9e5244986313
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-02-06 11:02:04 +0200

    Add animation when CV Layout View is changed

--- src/components/Preview/index.jsx
+++ src/components/Preview/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import VerticalView from "./VerticalView/index";
 import HorizontalView from "./HorizontalView/index";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Preview = ({
   componentRef,
@@ -13,44 +14,51 @@ const Preview = ({
 }) => {
   return (
     <main className="px-40">
-      <div
-        ref={componentRef}
-        style={{ fontFamily: layout.bodyFont }}
-        className="w-[800px] h-[1128px] bg-white shadow-lg"
-      >
-        {layout.view === "vertical" && (
-          <VerticalView
-            layout={layout}
-            personalInfo={personalInfo}
-            contactInfo={contactInfo}
-            workExperience={workExperience}
-            education={education}
-            skills={skills}
-          />
-        )}
-        {layout.view === "horizontalLeft" && (
-          <HorizontalView
-            display={"left"}
-            layout={layout}
-            personalInfo={personalInfo}
-            contactInfo={contactInfo}
-            workExperience={workExperience}
-            education={education}
-            skills={skills}
-          />
-        )}
-        {layout.view === "horizontalRight" && (
-          <HorizontalView
-            display={"right"}
-            layout={layout}
-            personalInfo={personalInfo}
-            contactInfo={contactInfo}
-            workExperience={workExperience}
-            education={education}
-            skills={skills}
-          />
-        )}
-      </div>
+      <AnimatePresence initial={false} mode="wait">
+        <motion.div
+          key={layout.view}
+          ref={componentRef}
+          style={{ fontFamily: layout.bodyFont }}
+          className="w-[800px] h-[1128px] bg-white shadow-lg"
+          initial={{ x: -200, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          exit={{ x: 200, opacity: 0 }}
+          transition={{ duration: 0.2 }}
+        >
+          {layout.view === "vertical" && (
+            <VerticalView
+              layout={layout}
+              personalInfo={personalInfo}
+              contactInfo={contactInfo}
+              workExperience={workExperience}
+              education={education}
+              skills={skills}
+            />
+          )}
+          {layout.view === "horizontalLeft" && (
+            <HorizontalView
+              display={"left"}
+              layout={layout}
+              personalInfo={personalInfo}
+              contactInfo={contactInfo}
+              workExperience={workExperience}
+              education={education}
+              skills={skills}
+            />
+          )}
+          {layout.view === "horizontalRight" && (
+            <HorizontalView
+              display={"right"}
+              layout={layout}
+              personalInfo={personalInfo}
+              contactInfo={contactInfo}
+              workExperience={workExperience}
+              education={education}
+              skills={skills}
+            />
+          )}
+        </motion.div>
+      </AnimatePresence>
     </main>
   );
 };
